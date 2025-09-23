ba577fef2bb775b2f2bb3f93a763f423b48b36e9
LizD <elizabethlizdunn@gmail.com>
2025-09-23 11:25:34 +1000

    Update and rename 7_20250308 exporting adipocyte geojson file to 7_20250308 exporting adipocyte geojson file.groovy

---
+++ IMC Qupath Scripts/7_20250308 exporting adipocyte geojson file.groovy
@@ -0,0 +1,10 @@
+def detections = getDetectionObjects()
+def name = getCurrentImageName()
+mkdirs("D:/ElizabethD/20241227 Qupath batch IMC project/adipocyte geojson files_2")
+def path = "D:/ElizabethD/20241227 Qupath batch IMC project/adipocyte geojson files_2"+ "/" +name + ".geojson"
+
+// 'FEATURE_COLLECTION' is standard GeoJSON format for multiple objects
+//exportObjectsToGeoJson(detections, path, "FEATURE_COLLECTION")
+
+// The same method without the 'FEATURE_COLLECTION' parameter outputs a simple JSON object/array
+exportObjectsToGeoJson(detections, path)
